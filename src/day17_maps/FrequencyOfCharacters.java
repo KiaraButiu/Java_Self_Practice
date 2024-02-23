bcb82ea72050f3ff77c4768de2b16ec0690c53bc
kiara <kikilorena49@yahoo.com>
2024-02-23 17:01:35 -0500

    week 17

---
+++ src/day17_maps/FrequencyOfCharacters.java
@@ -0,0 +1,33 @@
+package day17_maps;
+
+import java.rmi.MarshalledObject;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+
+       String str = "bbcccaaaaa";
+
+       Map<Character, Integer> output = new LinkedHashMap<>();
+
+        for (char each : str.toCharArray()) {       // to access each character
+
+            if (output.containsKey(each)){                 // if map contains character
+                output.put(each, output.get(each) + 1);     // add each same character if it contains
+            }else{
+                output.put(each, 1);                    // else each character remains 1
+            }
+        }
+        System.out.println(output);
+    }
+
+}
+/* Create a class named FrequencyOfCharacters and write a program that returns the frequency of each
+character in a given string as a map, without using nested loops.
+         Example:
+               string = "bbcccaaaaa"
+
+         Output:
+               {b=2, c=3, a=5}*/
---
+++ src/day17_maps/InvertMap.java
@@ -0,0 +1,34 @@
+package day17_maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InvertMap {
+    public static void main(String[] args) {
+
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"a");
+        map.put(2,"b");
+        map.put(3,"c");
+
+        Map<String, Integer> invertMap = new HashMap<>();
+
+        for (Map.Entry<Integer, String> eachEntry : map.entrySet()) {
+            String key = eachEntry.getValue();
+            int value = eachEntry.getKey();
+
+            invertMap.put(key,value);
+        }
+        System.out.println(invertMap);
+    }
+}
+/* *2. Create a class named InvertMap and write a program that returns a new map which is the
+inverted version of the provided map, meaning the keys become values and the values become keys.
+It is assumed that there are no duplicate values in the original map.
+
+         Example:
+               map = {1="a", 2="b", 3="c"}
+
+         Output:
+               {"a"=1, "b"=2, "c"=3}
+*/
